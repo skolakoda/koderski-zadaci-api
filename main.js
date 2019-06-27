d7a2b529906bff6c9a404e95f1f03ea03f7728b2
salEZY <bobicaleks@yahoo.com>
2019-06-27 18:56:10 +0200

    create server and install dependencies

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = 8080
+
+const userRouter = require('./routes/users/index')
+
+// Config
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Routes
+app.use('/users', userRouter)
+app.get('/', (req, res) => res.send('Dobrodosli na Koderski-Zadaci-API!'))
+
+
+//Server
+app.listen(port, () => console.log(`Server started at port: ${port}`)
+)
