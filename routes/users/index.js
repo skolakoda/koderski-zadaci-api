9b62272de2f45c1c87cad332b77b5b3993bc5faa
salEZY <bobicaleks@yahoo.com>
2019-06-27 19:20:12 +0200

    dodata logout ruta

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-const GitHubStrategy = require('passport-github2').Strategy;
+//const GitHubStrategy = require('passport-github2').Strategy
 
 // Passport session setup
-passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
+passport.serializeUser((user, done) => {
+    done(null, user)
+  })
   
-  passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-  });
+  passport.deserializeUser((obj, done) => {
+    done(null, obj)
+  })
 // Inicijalizacija Passport-a
 router.use(passport.initialize())
 router.use(passport.session())
 
 
+
+// Passport odradjuje sve , nece biti potrebe izdvajati logout rutu!
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
   
 module.exports = router
